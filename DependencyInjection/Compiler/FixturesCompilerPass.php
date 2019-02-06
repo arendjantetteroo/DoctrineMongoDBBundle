22d4babccc4b2dee6e9ba2e458019fcd205178bf
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2019-02-06 09:52:16 +0100

    CS fixes

--- DependencyInjection/Compiler/FixturesCompilerPass.php
+++ DependencyInjection/Compiler/FixturesCompilerPass.php
@@ -18,9 +18,6 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-/**
- *
- */
 final class FixturesCompilerPass implements CompilerPassInterface
 {
     const FIXTURE_TAG = 'doctrine.fixture.odm';
