3be6248cef27e154618c1b6567be92d14f322ef9
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2019-02-06 12:14:04 +0100

    Cleanup loader interface

--- Loader/SymfonyFixturesLoaderInterface.php
+++ Loader/SymfonyFixturesLoaderInterface.php
@@ -4,18 +4,7 @@ declare(strict_types=1);
 
 namespace Doctrine\Bundle\MongoDBBundle\Loader;
 
-use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\FixturesCompilerPass;
-use Doctrine\Bundle\MongoDBBundle\Fixture\FixtureGroupInterface;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\DataFixtures\FixtureInterface;
-use LogicException;
-use ReflectionClass;
-use RuntimeException;
-use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader;
-use function array_key_exists;
-use function array_values;
-use function get_class;
-use function sprintf;
 
 interface SymfonyFixturesLoaderInterface
 {
